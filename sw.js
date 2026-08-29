29e3f04a2c50e86124ed78427bead84930ba1f6b
jiazhe2020 <88466713+jiazhe2020@users.noreply.github.com>
2026-08-29 20:59:48 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'xiaopai-edu-v7';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './assets/food-pumpkin-porridge.jpg',
+  './assets/food-egg-custard.jpg',
+  './assets/food-tomato-tofu.jpg',
+  './assets/food-fish-balls.jpg',
+  './assets/food-banana-pancake.jpg',
+  './assets/food-veg-egg-pancake.jpg'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS).catch(function() {});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).then(function(resp) {
+        return resp;
+      }).catch(function() {
+        return cached;
+      });
+    })
+  );
+});
